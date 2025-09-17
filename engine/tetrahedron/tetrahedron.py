98c30fa43f69668262a6e56d5e34e957f068d689
Iongshiba <trandanghienlong2004@gmail.com>
2025-09-17 21:43:45 +0700

    refactoring

---
+++ engine/tetrahedron/tetrahedron.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+from OpenGL import GL
+
+from libs.buffer import VBO, VAO, EBO
+from libs.shader import Shader, ShaderProgram
+
+
+class Tetrahedron:
+    def __init__(self, vertex_file, fragment_file):
+        self.vertices = [
+            [-0.5, -0.5, 0.0],
+            [0.5, -0.5, 0.0],
+            [0.0, 0.5, 0.0],
+            [0.0, 0.0, 0.5],
+        ]
+        self.colors = [
+            [1.0, 0.0, 0.0],
+            [1.0, 1.0, 0.0],
+            [1.0, 0.0, 1.0],
+            [0.0, 1.0, 0.0],
+        ]
+        self.vertices = np.array(self.vertices, dtype=np.float32)
+        self.colors = np.array(self.colors, dtype=np.float32)
+
+        vertex_shader = Shader(vertex_file)
+        fragment_shader = Shader(fragment_file)
+        self.shader_program = ShaderProgram()
