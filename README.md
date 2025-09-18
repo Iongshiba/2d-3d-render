6790dc1a96ba3f2c69e9bd0cf10a949e815338cd
Iongshiba <trandanghienlong2004@gmail.com>
2025-09-18 08:32:41 +0700

    coding

---
+++ README.md
@@ -0,0 +1,1 @@
+docker run -it --rm -v ${PWD}:/app -w /app node:22-alpine sh
--- libs/__init__.py
+++
@@ -1,8 +0,0 @@
-# from .buffer import VAO
-# from .shader import Shader, Program
-
-# __all__ = [
-#     "VAO",
-#     "Shader",
-#     "Program",
-# ]
